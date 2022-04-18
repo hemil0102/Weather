bdea7908a7eb2b6137e26a9f4fcc51887e8455d4
hemil0102 <hemil0102@gmail.com>
2022-04-18 16:24:51 +0900

    어제 날린 것 복구 완료

--- Weather/Controller/EditAlarmVC.swift
+++ Weather/Controller/EditAlarmVC.swift
@@ -9,16 +9,25 @@ import UIKit
 
 class EditAlarmVC: UIViewController {
     
+    //[Harry] 변수 선언 및 정의
+    let EditAlarmBrain = AlarmBrain()
     @IBOutlet weak var repeatingDayOfWeekSwitch: UISwitch!
     @IBOutlet var DayOfWeekBtns: [UIButton]!
-    @IBOutlet weak var settingPlaceSwitch: UISwitch!
-    @IBOutlet weak var inputPlaceTextField: UITextField!
     @IBOutlet weak var inputALineMemoTextField: UITextField!
+    @IBOutlet var TodayMarks: [UILabel]!
     
+    
+    //[Harry] 함수 선언 및 정의
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //[Harry] 현재 날짜 및 시간 정보를 제대로 가져오는지 확인
+        EditAlarmBrain.getCurrentDT()
+        //[Harry] 요일 인덱스 확인
+        EditAlarmBrain.getWeekDayIndex()
+        //[Harry] today 문구를 오늘 요일에 표시
+        letTodayMarkAt()
     }
+    
     
     @IBAction func saveBtnAct(_ sender: UIButton) {
         //[Walter] 상태 저장
@@ -30,22 +39,23 @@ class EditAlarmVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func switchingActivationDayOfWeekBtns(_ sender: UISwitch) {
-        self.activationBtns(isOn: sender.isOn)
-    }
-    
-    func activationBtns(isOn: Bool) {
-        for btns in self.DayOfWeekBtns {
-            btns.isEnabled = isOn
-        }
-    }
-    
-    @IBAction func switchingActivationSettingPlace(_ sender: UISwitch) {
-        //[Walter] inputPlaceTextField 활성화/비활성화
-        self.inputPlaceTextField.isEnabled = sender.isOn
-    }
     
     @IBAction func selectedDayOfWeek(_ sender: UIButton) {
         
+    }
+    
+    //[Harry] 현재 요일에 today 마크를 표시하기
+    func letTodayMarkAt() {
+        
+        let weekDayChecker = EditAlarmBrain.getWeekDayIndex()
+        
+        for i in 0..<TodayMarks.count {
+            
+            if i == weekDayChecker {
+                TodayMarks[i].textColor = .black
+            } else {
+                TodayMarks[i].textColor = .white
+            }
+        }
     }
 }
--- Weather/Model/AlarmBrain.swift
+++ Weather/Model/AlarmBrain.swift
@@ -10,4 +10,55 @@ import Foundation
 // 알람 기능 구현부
 struct AlarmBrain {
     var myAlarm: AlarmModel? //객체화
+    
+//[Harry] 현재 날짜와 시간을 가져오는 함수
+    func getCurrentDT() -> String {
+        // [date 객체 사용해 현재 날짜 및 시간 24시간 형태 출력 실시]
+        let nowDate = Date() // 현재의 Date 날짜 및 시간
+        let dateFormatter = DateFormatter() // Date 포맷 객체 선언
+        dateFormatter.locale = Locale(identifier: "ko") // 한국 지정
+        
+        dateFormatter.dateFormat = "yyyy.MM.dd kk:mm:ss E요일" // Date 포맷 타입 지정
+        let date_string = dateFormatter.string(from: nowDate) // 포맷된 형식 문자열로 반환
+
+        print("")
+        print("===============================")
+        print("날짜 :: ", date_string)
+        print("===============================")
+        print("")
+            
+        return date_string
+    }
+    
+//[Harry] 현재 요일을 구분하는 Index를 반환해주는 함수
+    func getWeekDayIndex() -> Int {
+        
+        let currentDay = getCurrentDT()[getCurrentDT().index(getCurrentDT().endIndex,offsetBy: -3)]
+        var currentDayIndex: Int
+        
+        if currentDay == "월" {
+            currentDayIndex = 0
+        } else if currentDay == "화" {
+            currentDayIndex = 1
+        } else if currentDay == "수" {
+            currentDayIndex = 2
+        } else if currentDay == "목" {
+            currentDayIndex = 3
+        } else if currentDay == "금" {
+            currentDayIndex = 4
+        } else if currentDay == "토" {
+            currentDayIndex = 5
+        } else {
+            currentDayIndex = 6
+        }
+        
+        print("")
+        print("===============================")
+        print("요일 :: ", currentDay)
+        print("요일Index :: ", currentDayIndex)
+        print("===============================")
+        print("")
+        
+        return currentDayIndex
+    }
 }

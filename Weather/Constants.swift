607e3755b64f3f672db1442e9636a588ba5b5a6f
Walter-Jo <ys.mobility@gmail.com>
2022-04-18 16:28:02 +0900

    add SearchAreaModla files and others..

--- Weather/Constants.swift
+++ Weather/Constants.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 struct Keys {
+    //[Walter] 광고 관련 테스트에만 쓰이는 키
     struct GoogleTestAD {
         static let testBannerADKey = "ca-app-pub-3940256099942544/2934735716"
         static let testNativeADKey = "ca-app-pub-3940256099942544/3986624511"
     }
     
+    //[Walter] api key
     struct ApiId {
         static let weatherAppId = "d03b61bd0fafa57cf46bd374eb796830"
         static let airPolutionId = "d0aedaa971b852d7c7d77cb006728cf6" //[jongmin] 미세먼지용 키
@@ -23,6 +25,14 @@ struct Keys {
     static let alarmCellOneIdentifier = "AlarmCellOneIdentifier"
     static let alarmCellTwoNibName = "AlarmCell2"
     static let alarmCellTwoIdentifier = "AlarmCellTwoIdentifier"
+    
+    //[Walter] 지역검색 모달 관련 Identifier
+    struct searchArea {
+        static let segueId = "HomeToSearchAreaModel"
+        static let storyboardId = "searchAreaModal"
+        static let cellName = "SearchAreaCell"
+        static let cellId = "SearchAreaCell"
+    }
 }
 
 struct ViewIdentifier {
--- Weather/Controller/SearchModalVC.swift
+++ Weather/Controller/SearchModalVC.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class SearchModalVC: UIViewController {
 
+    @IBOutlet weak var areaTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.areaTableView.delegate = self
+        self.areaTableView.dataSource = self
+        self.areaTableView.register(UINib(nibName: Keys.searchArea.cellName, bundle: nil), forCellReuseIdentifier: Keys.searchArea.cellId)
+    }
+}
+
+//MARK: - TableViewDelegate
+extension SearchModalVC: UITableViewDelegate {
+    
+}
+
+//MARK: - TableViewDataSource
+extension SearchModalVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Keys.searchArea.cellId) as! SearchAreaCell
         
+        cell.weatherIconImageView.image = UIImage(named: "cloud.bolt.rain")
+        cell.showCurrLocationLabel.isHidden = true
+        cell.areaNameLabel.text = "수원시 구운동"
+        cell.currTempLabel.text = "17℃"
+        cell.currCloudLabel.text = "강우량 0%"
+        
+        return cell
     }
 }

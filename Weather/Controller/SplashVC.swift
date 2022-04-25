695704d79f3b8ec2fe5c5c28729871d962f4e966
Walter-Jo <ys.mobility@gmail.com>
2022-04-25 21:59:05 +0900

    add SplachVC instead LaunchScreen

---
+++ Weather/Controller/SplashVC.swift
@@ -0,0 +1,15 @@
+//
+//  SplashVC.swift
+//  Weather
+//
+//  Created by Walter J on 2022/04/25.
+//
+
+import Foundation
+import UIKit
+
+class SplashVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}

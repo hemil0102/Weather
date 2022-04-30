03946e5e2b840ecc472ca3e3ac72fa176d70f515
Walter-Jo <ys.mobility@gmail.com>
2022-04-30 19:58:11 +0900

    add hourly weather

--- Weather/Model/WeatherData.swift
+++ Weather/Model/WeatherData.swift
@@ -7,31 +7,38 @@
 
 import Foundation
 
-// MARK: - [Walter] 현재 날짜 데이터 구조체
+// MARK: - [Walter] 오픈 웨더 데이터 구조체
 struct WeatherData: Codable {
     let current: Current            // 지역명
+    let hourly: [Hourly]
     let daily: [Daily]
 }
 
+// MARK: - [Walter] 현재 날씨
 struct Current: Codable {
     let temp: Double
-    let sunrise: Int
-    let sunset: Int
     let humidity: Int
     let clouds: Int
     let wind_speed: Double
     let weather: [Weather]
+    let rain: Rain?
 }
 
-struct Weather: Codable {
-    let id: Int
-    let description: String
+// MARK: - [Walter] 현재 날씨 - 시간별
+struct Hourly: Codable {
+    let dt: Int
+    let temp: Double
+    let feels_like: Double
+    let weather: [Weather]
+    let pop: Double                 //강수 확률
 }
 
+//MARK: - [Walter] 주간 날씨
 struct SevenDaysWeather: Codable {
     let daily: [Daily]
 }
 
+//MARK: - [Walter] 주간 날씨
 struct Daily: Codable {
     let dt: Int
     let temp: Temp
@@ -42,6 +49,12 @@ struct Daily: Codable {
     let uvi: Double
 }
 
+//MARK: - [Walter] 날씨 상태
+struct Weather: Codable {
+    let id: Int
+    let description: String
+}
+
 struct Temp: Codable {
     let day: Double
     let min: Double
@@ -49,6 +62,14 @@ struct Temp: Codable {
     let night: Double
     let eve: Double
     let morn: Double
+}
+
+//MARK: - [Walter] 비
+struct Rain: Codable {
+    enum CodingKeys: String, CodingKey {
+        case h1 = "1h"
+    }
+    let h1: Double
 }
 
 // MARK: - [Jongmin] 미세먼지 데이터 구조체
@@ -64,5 +85,3 @@ struct Components: Codable {
     let pm2_5: Double
     let pm10: Double
 }
-
-// MARK: - [Walter] 지난 시간 날짜 데이터 구조체
